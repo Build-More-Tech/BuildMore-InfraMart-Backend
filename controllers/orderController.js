0d4089d80e51f70c818ad9f92d9e3775b3f8e2f0
Subhrajit <er.subhrajitm@gmail.com>
2026-05-01 09:23:36 +0530

    ordercont-1

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,4 @@
-const Order = require('../models/orderModel');
+const Order = require('../models/OrderModel');
 const Product = require('../models/ProductModel');
 const { sendOrderConfirmation, sendOrderStatusUpdate } = require('../services/emailService');
 const User = require('../models/userModel');
